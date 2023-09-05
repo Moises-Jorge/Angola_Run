0d64cd13a7fea21e71cf95e3b50223d06b0c80a9
Moisés Jorge <moisesjorge30@gmail.com>
2023-09-05 12:24:44 +0100

    I added the player to our game, but it's not the original player, it's just a test
    Also i changed the background image to a test background

--- JS/Main.js
+++ JS/Main.js
@@ -5,7 +5,16 @@ window.onload = function () {
         type: Phaser.AUTO,
         width: 800,
         height: 600,
-        scene: [Scene01]
+        scene: [Scene01],
+
+        // Configurando o motor de Física, que vai permitir ao personagem colidir com os outros objectos
+        physics: {
+            default: 'arcade', // Nome do motor de física que estamos usando
+            arcade: { // Configurações do Arcade
+                gravity: {y: 1000} // Estamos configurando apenas a gravidade no eixo "y"
+            } 
+        },
+        pixelArt: true // Para manter a qualidade e a nitidez das imagens quando as suas escalas forem alteradas
     }
 
     game = new Phaser.Game(config)
--- JS/Scene01.js
+++ JS/Scene01.js
@@ -4,13 +4,21 @@ class Scene01 extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('background', 'img/background.jpg') // Carregando a imagem de background
+        this.load.image('background', 'img/sky.png') // Carregando a imagem de background (DEPOIS SUBSTITUIR COM A IMAGEM ORIGINAL DO FUNDO PARA ESSE JOGO)
+
+        this.load.spritesheet('player', 'img/player.png', {frameWidth: 32, frameHeight: 32}) // Carregando a spritesheet(imagem com várias posições) do jogador/personagem. O terceiro atributo refere-se ao tamanho das frames(o tamanho de cada imagem contida no sprite) (DEPOIS SUBSTITUIR COM A IMAGEM ORIGINAL DO PERSONAGEM PARA ESSE JOGO)
     }
 
     create() {
+        // BACKGROUND
         this.background = this.add.image(0, 0, 'background').setOrigin(0,0) // Adicionando a imagem de BG no jogo e ajustando o seu ponto de origem(canto superior esquerdo)
         this.background.displayWidth = 800 // Ajustando a largura da imagem para se adequar à tela
         this.background.displayHeight = 600 // A justando a altura da imagem para se adequar à tela
+
+        // PLAYER
+        this.player = this.physics.add.sprite(50, 500, 'player') // Adicionando o jogador no jogo
+        .setCollideWorldBounds(true) // Comando que permite que o personagem colida com o solo (pq devido a gravidade, ele pode simplesmente não parar na tela)
+        .setScale(2, 2) // Alterando o tamanho do jogador
     }
 
     update() {}
