ecc887e6563b14cd11f99ac6b09cedc463e7d91a
Moisés Jorge <moisesjorge30@gmail.com>
2023-09-06 17:09:27 +0100

    I created and added functionality to keyboards

--- JS/Scene01.js
+++ JS/Scene01.js
@@ -26,10 +26,31 @@ class Scene01 extends Phaser.Scene {
         // Adicionando o personagem no jogo e aumentando a sua escala
         this.player = this.physics.add.sprite(50, 500, 'player')
         .setCollideWorldBounds(true).setScale(2).setVelocityX(150)
+
+        // Criando e adicionando funções às teclas (Para interagir com o personagem e para iniciar o jogo)
+        this.player.canJump = true // variável que controla o salto do personagem
+        this.keyBoard = this.input.keyboard.createCursorKeys()
     }
 
     /**
      * Método usado para actualizar o jogo, ou seja, alterar as carecterísticas padrão de alguns objectos
      */
-    update() {}
+    update() {
+        // Movimentação do jogador no eixo "X": APENAS UM TESTE, DEPOIS REMOVER
+        if (this.keyBoard.left.isDown) {
+            this.player.setVelocityX(-150)
+        } else {
+            this.player.setVelocityX(150)
+        }
+
+        // Movimentação do jogador no eixo "Y": Saltar e baixar
+        if (this.keyBoard.up.isDown && this.player.canJump) {
+            this.player.setVelocityY(-500)
+            this.player.canJump = false
+        }
+        // Recuperando o valor "True" para o personagem poder pular novamente
+        if (!this.keyBoard.up.isDown && !this.player.canJump) {
+            this.player.canJump = true
+        }
+    }
 }
